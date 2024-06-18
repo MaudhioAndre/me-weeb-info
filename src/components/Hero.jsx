3f601c2fa92d4bfc5a01c2b35cb4eca6101f00d5
Maudhio Andre <maudhioa@gmail.com>
2024-06-18 13:58:42 +0700

    try

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { API_URL } from "../api/jikanApi";
+import React from "react";
 
 import "../assets/style/front.scss"
 import { Link } from "react-router-dom";
 
 export default function Hero({randomAnime}) {
-  // const [randomAnime, setRandomAnime] = useState({});
-
-  // console.log("Dashboard");
-  // console.log(randomAnime);
-  // console.log("Dashboard");
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     console.log("getData");
-  //     await axios
-  //       .get(`${API_URL}/random/anime`)
-  //       .then((response) => {
-  //         console.log(response);
-  //         setRandomAnime(response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   };
-
-  //   getData();
-  // }, []);
 
   const isObjectEmpty = (objectName) => {
     return Object.keys(objectName).length === 0;
