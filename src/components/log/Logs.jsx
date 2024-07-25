30dae24ef6567af1d0f3212cb1dc071d450e6259
Maudhio Andre <maudhioa@gmail.com>
2024-07-25 21:15:42 +0700

    .

--- src/components/log/Logs.jsx
+++ src/components/log/Logs.jsx
@@ -1,19 +1,10 @@
- const Logs = (func, user) => {
-    console.log("kesini");
-    console.log(user);
-    const time = user ? 0 : 2000
-    console.log(time);
-    return setTimeout(() => {
-      func(true);
-    }, time);
-  }
-  
+import { worker } from "driftless";
 
-// export default function log(func) {
-//     return setTimeout(() => {
-//       func(true);
-//     }, 2000);
-//   }
-  
+const Logs = (func, user) => {
+  const int = user ? 0 : 2000;
+  return worker(() => {
+    func(true);
+  }, int);
+};
 
-export default Logs
+export default Logs;
